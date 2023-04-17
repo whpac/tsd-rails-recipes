31ae33250b7eeb02d65eb1b052c1bbfe31089b72
Michał Maćkowiak <michal.mackowiak@netguru.pl>
2023-04-17 14:34:03 +0000

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: "user@example.com", password: "password", password_confirmation: "password")
+
+Recipe.create(title: "Pancakes", description: "Delicious pancakes")
+Recipe.create(title: "Pasta", description: "Delicious pasta")
+Recipe.create(title: "Pizza", description: "Delicious pizza")

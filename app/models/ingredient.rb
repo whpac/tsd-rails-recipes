0e2c8228bc8899f2e7871bb92f1d065bf62ac53a
Marcin Szwarc <marcinszwarc@hotmail.com>
2024-04-23 16:19:52 +0000

    Task 5

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,11 @@ class Ingredient < ApplicationRecord
   has_one_attached :photo do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
   end
+  validate :cannot_have_more_than_3_ingrediends
+
+  def cannot_have_more_than_3_ingrediends
+    if recipe.ingredients.size > 3
+      errors.add(:recipe ,'has too many ingredients')
+    end
+  end
 end
